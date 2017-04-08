93520841b691e64b35efdbb9b20dec8c0b867c08
samson_oregon <zebraxlj@gmail.com>
2017-04-08 00:52:35 -0400

    [major] load_occurrence_table, get_total_char_count - load from multiple file and merge data

--- proj2/util.py
+++ proj2/util.py
@@ -1,25 +1,38 @@
 import csv
 import re
 
-def load_occurrence_file(file_path, num_word=True, num_file=False):
+''' able to load multiple file and merge the occurrence table '''
+def load_occurrence_file(file_pathes, num_word=True, num_file=False):
+	assert isinstance(file_pathes, (list, tuple)), 'file_pathes must be a list or tuple'
 	occurrence = dict()
-	with open(file_path, 'r') as file:
-		_ = file.readline()
-		for line in file:
-			tokens = re.split('[\t|\n| ]+', line)
-			if num_word:
-				occurrence[tokens[0]] = float(tokens[1])
-			elif num_file:
-				occurrence[tokens[0]] = float(tokens[2])
+	for file_path in file_pathes:
+		with open(file_path, 'r') as file:
+			_ = file.readline()
+			for line in file:
+				tokens = re.split('[\t|\n| ]+', line)
+				if num_word:
+					if tokens[0] in occurrence:
+						#print tokens[0], occurrence[tokens[0]], occurrence[tokens[0]]+float(tokens[1])
+						occurrence[tokens[0]] += float(tokens[1])
+					else:
+						occurrence[tokens[0]] = float(tokens[1])
+				elif num_file:
+					if tokens[0] in occurrence:
+						#print tokens[0], occurrence[tokens[0]], occurrence[tokens[0]]+float(tokens[1])
+						occurrence[tokens[0]] += float(tokens[2])
+					else:
+						occurrence[tokens[0]] = float(tokens[2])
 	return occurrence
 
-def get_total_char_count(char_occurrence_file):
-        total_char_count = 0
-	with open(char_occurrence_file, 'r') as file:
-		_ = file.readline()
-		for line in file:
-			tokens = re.split('[\t|\n| ]+', line)
-			total_char_count += float(tokens[1])
+def get_total_char_count(char_occurrence_files):
+	total_char_count = 0
+	for char_occurrence_file in char_occurrence_files:
+                print char_occurrence_file
+		with open(char_occurrence_file, 'r') as file:
+			_ = file.readline()
+			for line in file:
+				tokens = re.split('[\t|\n| ]+', line)
+				total_char_count += float(tokens[1])
 	return total_char_count
 
 def test_probability_table(table):
@@ -30,3 +43,11 @@ def test_probability_table(table):
 		sum += v
 	print 'sum of p: ', sum
 
+
+if __name__ == '__main__':
+	single = 'word_occurrence/einstein_relativity_nows'
+	multiple = ['word_occurrence/einstein_relativity_nows', 'word_occurrence/california_mexican_spanish_cook_book_nows']
+	print single
+	#temp1 = load_occurrence_file([single])
+	temp2 = load_occurrence_file(multiple)
+	pass
